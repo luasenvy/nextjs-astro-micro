f49ef9b9cc0f2931ee5be92f8f724353ff8288bf
luasenvy <luas.envy@gmail.com>
2024-10-02 15:53:01 +0900

    fix: mdx.d.ts

---
+++ mdx.d.ts
@@ -0,0 +1,21 @@
+declare module "*.mdx" {
+  import type { Element, MDXProps } from "mdx/types";
+
+  interface Frontmatter {
+    slug: string;
+    title: string;
+    subtitle: string;
+    image: string;
+    author: string;
+    date: string;
+    categories: Array<string>;
+    featured: boolean;
+    draft: boolean;
+    content: string;
+  }
+
+  function MDXContent(props: MDXProps): Element;
+  const metadata: Frontmatter;
+
+  export { metadata, MDXContent as default };
+}
